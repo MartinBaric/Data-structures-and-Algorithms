4741c315a29468f8bd54e500bccdf7c94753cedb
Martin B. <uajew2@gmail.com>
2024-05-06 17:55:20 +0200

    Template and first Test object

--- Insertsort.cpp
+++ Insertsort.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <list>
+
 using namespace std;
+
+void bubbleSort(list<int>& unsorted)
+{
+    // Dimzog's task
+}
+
+void insertSort(list<int>& unsorted)
+{
+    // Dimzog's task
+}
+
+void quickSort(list<int>& unsorted)
+{
+    // Dimzog's task
+}
+
 
 int main()
 {
+    fstream myfile;
+    myfile.open("Test 1.csv");
+    string line;
+
+    list<int> elements;
+
+    while(getline(myfile, line))
+        elements.push_back(stoi(line));
+
+    quickSort(elements);
+
+    myfile.close();
+
     return 0;
 }
