3c70e56b13fc03c961dc1b51173ff758754fbc70
Martin B. <uajew2@gmail.com>
2024-05-07 19:45:54 +0200

    Added time stamps

--- Linked lists/LinkedLists.cpp
+++ Linked lists/LinkedLists.cpp
@@ -2,21 +2,48 @@
 #include <fstream>
 #include <string>
 #include <list>
+#include <chrono>
 
 using namespace std;
+using namespace chrono;
+
+struct Timer
+{
+    time_point<high_resolution_clock> start, end;
+    duration<float> duration;
+    string name_of_algorithm;
+
+    Timer(string name)
+    {
+        start = high_resolution_clock::now();
+        name_of_algorithm = name;
+    }
+
+    ~Timer()
+    {
+        end = high_resolution_clock::now();
+        duration = end - start;
+        float ms = duration.count()*1000.0f;
+
+        cout << "The algorithm " << name_of_algorithm << " took " << ms << "ms to execute.";
+    }
+};
 
 void bubbleSort(list<int>& unsorted)
 {
+    Timer("Bubble Sort");
     // Dimzog's task
 }
 
 void insertSort(list<int>& unsorted)
 {
+    Timer("Insert Sort");
     // Dimzog's task
 }
 
 void quickSort(list<int>& unsorted)
 {
+    Timer("Quick Sort");
     // Dimzog's task
 }
 
@@ -32,7 +59,7 @@ int main()
     while(getline(myfile, line))
         elements.push_back(stoi(line));
 
-    quickSort(elements);
+    bubbleSort(elements);
 
     myfile.close();
 
