d0cda31aee2f5d37cc0c98c5afe63d33d426f2a8
Dmitrij Sitenko <dmitrij.sitenko@iwr.uni-heidelberg.de>
2024-05-16 22:40:44 +0200

    Addd_Quicksort_and_swap_changes_for_visualization

--- Linked lists/LinkedLists.cpp
+++ Linked lists/LinkedLists.cpp
@@ -4,16 +4,22 @@
 #include <list>
 #include <chrono> // For time measuring
 #include <omp.h> // OpenMultiProcessing library for parallel programming
+#include <random>
 
 using namespace std;
 using namespace chrono;
 
 const int NUM_THREADS = 4;
 
+struct Swap_Pos{
+    int Old_pos; 
+    int New_pos;  
+};
+
 struct Timer
 {
     time_point<high_resolution_clock> start, end;
-    duration<float> duration;
+    duration<float> elapsed_time;
     string name_of_algorithm;
 
     Timer(string name)
@@ -25,8 +31,8 @@ struct Timer
     ~Timer()
     {
         end = high_resolution_clock::now();
-        duration = end - start;
-        float ms = duration.count()*1000.0f;
+        elapsed_time = end - start;
+        float ms = elapsed_time.count()*1000.0f;
 
         cout << "The algorithm " << name_of_algorithm << " took " << ms << "ms to execute.";
     }
@@ -44,33 +50,77 @@ void insertSort(list<int>& unsorted)
     // Dimzog's task
 }
 
-void quickSort(list<int>& unsorted)
+void quickSort(int* unsorted,int left, int right,list<Swap_Pos>& Swap_List)
 {
     Timer("Quick Sort");
-    // Dimzog's task
+    // Check Length
+    if(left >= right)
+        return;
+    // Pick a random Pivot Element within left and right range 
+    int swap_temp;
+    int pivot = left + rand()%(right-left);
+    int pivot_el = unsorted[pivot];
+    int i = left;
+    int j = right;
+    while(i<j){
+        while(unsorted[i] < pivot_el){
+            i++;
+        }
+        while(unsorted[j] > pivot_el){
+            j--;
+        }
+        if (i <= j)
+        {   
+            swap_temp = unsorted[i];
+            unsorted[i] = unsorted[j];
+            unsorted[j] = swap_temp;
+            Swap_Pos Pos;
+            Pos.Old_pos = i;
+            Pos.New_pos = j;
+            Swap_List.push_back(Pos);
+            // Continue with next index
+            i++;
+            j--;
+        }
+    }
+    quickSort(unsorted,left,j,Swap_List);
+    quickSort(unsorted,i,right,Swap_List);
 }
 
 
 int main()
 {
-    omp_set_num_threads(NUM_THREADS);
+    //omp_set_num_threads(NUM_THREADS);
     fstream myfile;
     myfile.open("Test 1.csv");
     string line;
-
     list<int> elements;
-
+    int i = 0;
+    list<Swap_Pos> Swap_List;
     while(getline(myfile, line))
         elements.push_back(stoi(line));
 
-    #pragma omp parallel
-    {
-        bubbleSort(elements);
-        insertSort(elements);
-        quickSort(elements);
+    // Conevert to Array for Quicksort Efficiency
+    int size_l = elements.size();
+    int* elements_array = (int*) malloc(size_l*sizeof(elements.front()));
+    for(int element: elements){
+        elements_array[i] = int(element);
+        i++;
     }
-
     myfile.close();
+    quickSort(&elements_array[0],0,elements.size(),Swap_List);
+    //#pragma omp parallel
+    //{
+        //bubbleSort(elements);
+        //insertSort(elements);
+     //   quickSort(&elements_array[0],0,i);
+    //}
+    for(int k = 0;k < elements.size();k++){
+       cout << elements_array[k] << "\n"; 
+    }
+    for (auto it = Swap_List.begin(); it != Swap_List.end(); ++it){
+    cout <<"(" << it->New_pos << "," << it->Old_pos << ")";
+    }
 
     return 0;
 }
